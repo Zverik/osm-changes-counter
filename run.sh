c5cb0f9d5acd673732429caaa775f96aafd6aff6
Ilya Zverev <zverik@textual.ru>
2021-09-21 16:54:28 +0300

    fix run errors

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 [ -z "${1-}" ] && echo "Usage: $0 psql_database_name [<regions.csv>]" && exit 1
 cd "$(dirname "$0")"
 PSQL=( psql "$1" -v ON_ERROR_STOP=1 )
+OVERPASS='http://overpass-api.de/api'
 PYTHON=venv/bin/python
 NEXT_TS="$(${PSQL[@]} -qAtc 'select ts + 1 from adiff_tracker_ts order by ts desc limit 1')"
 TS="$($PYTHON gen_adiff_timestamps.py)"
@@ -10,7 +11,7 @@ for ts in $(seq $NEXT_TS $TS); do
     download_ok=
     while [ -z "$download_ok" ]; do
         echo "$(date +%H:%M:%S): $ts ($($PYTHON gen_adiff_timestamps.py -$ts))"
-        curl -s "http://overpass-api.de/api/augmented_diff?id=$ts" > $ts.adiff
+        curl -s "$OVERPASS/augmented_diff?id=$ts" > $ts.adiff
         grep -q '<action type=' $ts.adiff && download_ok=yes
         [ -z "$download_ok" ] && sleep 20
     done
