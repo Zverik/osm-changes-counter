70a95c918d0f60aea94b28e922cc6d33fe92b268
Ilya Zverev <zverik@textual.ru>
2021-10-01 13:17:52 +0300

    Support csv input for stats

--- stats.sh
+++ stats.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -euo pipefail
-[ $# -lt 2 ] && echo "Usage: $0 psql_database_name <output.html> [<weights.lst> [<uids.csv>]]" && exit 1
+[ $# -lt 2 ] && echo "Usage: $0 {psql_database_name|csv_file} <output.html> [<weights.lst> [<uids.csv>]]" && exit 1
 cd "$(dirname "$0")"
-PSQL=( psql "$1" -v ON_ERROR_STOP=1 )
 PYTHON=venv/bin/python
 CSV_ARG=
 [ "${2##*.}" == "csv" ] && CSV_ARG="--csv"
-${PSQL[@]} -c "copy (select * from osc_tracker order by ts, osm_id, kind) to stdout (format csv, header)" \
-  | $PYTHON generate_user_stats.py $CSV_ARG ${3+-w "$3"} ${4+-u "$4"} -o "$2"
+if [ -e "$1" ]; then
+   $PYTHON generate_user_stats.py -i "$1" $CSV_ARG ${3+-w "$3"} ${4+-u "$4"} -o "$2"
+else
+  PSQL=( psql "$1" -v ON_ERROR_STOP=1 )
+  ${PSQL[@]} -c "copy (select * from osc_tracker order by ts, osm_id, kind) to stdout (format csv, header)" \
+    | $PYTHON generate_user_stats.py $CSV_ARG ${3+-w "$3"} ${4+-u "$4"} -o "$2"
+fi
